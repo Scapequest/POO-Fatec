5e1199ae83019e4e3438b09a095c48a01d736056
unknown <renanmastroto4@gmail.com>
2026-02-23 22:15:41 -0300

    Finalizado primeira atividade

---
+++ ClasseConta/Conta.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClasseConta
+{
+    public class Conta
+    {
+        // declaração de atributos
+        public int numero;
+        public string titular;
+        public double saldo;
+
+        // declaração de funções / métodos
+
+        public void Sacar(double ValorSacar){
+            saldo -= ValorSacar;
+        }
+        public void Depositar(){
+
+        }
+
+        public void MostrarAtributos(){
+            System.Console.WriteLine($"Número: {numero} \tTitular: {titular} \tSaldo: {saldo}");
+        }
+    }
+}
---
+++ ClasseConta/Program.cs
@@ -0,0 +1,18 @@
+﻿using ClasseConta;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Conta c1 = new Conta();
+        c1.numero = 1;
+        c1.titular = "Renan";
+        c1.saldo = 100;
+        c1.MostrarAtributos();
+        c1.Sacar(50);
+        c1.MostrarAtributos();
+
+        Conta c2 = new Conta();
+        c2.MostrarAtributos();
+
+    }
+}
